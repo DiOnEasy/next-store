160e5b2204b89ce83bd1c56c0ac7541c23dc933a
DiOnEasy <d11062002d@gmail.com>
2023-09-15 00:17:24 +0300

    project settings

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+	return <>Home page</>
+}
+
+export default HomePage
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from 'tailwindcss'
 
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+	transparent: twColors.transparent,
+	black: '#2e3239',
+	white: twColors.white,
+  secondary: '#161D25',
+  primary: '#ff9902',
+  'bg-color': '#f2f2f5',
+  aqua: '#268697'
+}
+
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		colors,
+		extend: {}
+	},
+	plugins: []
 }
 export default config
