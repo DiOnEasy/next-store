7f0fe59865b16ff17ee1b8edcdac3cdeb1b06b7c
DiOnEasy <d11062002d@gmail.com>
2023-09-20 21:01:17 +0300

    added types

---
+++ src/types/product.interface.ts
@@ -0,0 +1,20 @@
+import { ICategory } from './category.interface'
+import { IReview } from './review.interface'
+
+export interface IProduct {
+	id: number
+	createdAt: string
+
+	name: string
+	slug: string
+	description: string
+	price: number
+	images: string[]
+
+	reviews: IReview[]
+	category: ICategory
+}
+
+export interface IProductDetails {
+	product: IProduct
+}
