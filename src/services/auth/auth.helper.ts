eb172ce2a4f008bf7301a7b760eece9f2b014027
DiOnEasy <d11062002d@gmail.com>
2023-09-20 21:41:00 +0300

    auth & api interceptor

--- src/services/auth/auth.helper.ts
+++ src/services/auth/auth.helper.ts
@@ -1,14 +1,24 @@
 import { IAuthResponse, ITokens } from '@/store/user/user.interface'
 import Cookies from 'js-cookie'
 
+export const getAccessToken = async () => {
+	const accessToken = Cookies.get('accessToken')
+	return accessToken
+}
+
+export const getUserFromStorage = async () => {
+	return JSON.parse(localStorage.getItem('user') || '{}')
+}
+
 export const saveTokensStorage = (data: ITokens) => {
 	Cookies.set('accessToken', data.accessToken)
 	Cookies.set('refreshToken', data.refreshToken)
 }
 
-export const removeTokensStorage = () => {
+export const removeFromStorage = () => {
 	Cookies.remove('accessToken')
 	Cookies.remove('refreshToken')
+	localStorage.removeItem('user')
 }
 
 export const saveToStorage = (data: IAuthResponse) => {
